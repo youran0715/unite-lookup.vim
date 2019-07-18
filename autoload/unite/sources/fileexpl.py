6618bcaf9787c0b17ca83226036f9867a2576f7d
wuhong1 <wuhong1@cmschina.com.cn>
2019-07-18 09:17:36 +0800

    精简代码，只用python3

--- autoload/unite/sources/fileexpl.py
+++ autoload/unite/sources/fileexpl.py
@@ -8,94 +8,37 @@ import sys
 import time
 import os.path
 import fnmatch
-import locale
 
 lfEval = vim.eval
 
-if sys.version_info >= (3, 0):
-    # vim.command('echomsg "use python3"')
-    def lfEncode(str):
-        return str
-
-    def lfDecode(str):
-        return str
-else: # python 2.x
-    def lfEncode(str):
-        try:
-            if locale.getdefaultlocale()[1] is None:
-                return str
-            else:
-                return str.decode(locale.getdefaultlocale()[1]).encode(lfEval("&encoding"))
-        except ValueError:
-            return str
-        except UnicodeDecodeError:
-            return str
-
-    def lfDecode(str):
-        try:
-            if locale.getdefaultlocale()[1] is None:
-                return str
-            else:
-                return str.decode(lfEval("&encoding")).encode(
-                        locale.getdefaultlocale()[1])
-        except UnicodeDecodeError:
-            return str
-
-def writelist2file(file_path, file_list):
+def saveFileList(file_path, file_list):
     with open(file_path, 'w') as f:
         for item in file_list:
             f.write("%s\n" % os.path.relpath(item))
         f.close()
 
+def updateFileList(dir_path, file_path, wildignore, linksflag):
+    start_time = time.time()
+    file_list = []
+    for dir_path, dirs, files in os.walk(dir_path, followlinks = False
+            if linksflag == '0' else True):
+        dirs[:] = [i for i in dirs if True not in (fnmatch.fnmatch(i,j)
+                   for j in wildignore['dir'])]
+        for name in files:
+            if True not in (fnmatch.fnmatch(name, j)
+                            for j in wildignore['file']):
+                file_list.append(os.path.join(dir_path,name))
+            if time.time() - start_time > 120:
+                writelist2file(file_path, file_list)
+                return
+
+    saveFileList(file_path, file_list)
+
 def UnitePyGetFileList():
     dir_path = vim.eval('s:dir_path')
     file_path = vim.eval('s:file_path')
-    # vim.command('echomsg "dir_path:%s"' % dir_path)
-    # dir_path = "./"
-    start_time = time.time()
     wildignore = lfEval("g:lookupfile_WildIgnore")
+    linksflag = lfEval("g:lookupfile_FollowLinks")
 
-    # wildignore = {"dir":[], "file":[]}
-    file_list = []
-    for dir_path, dirs, files in os.walk(dir_path, followlinks = False
-            if lfEval("g:lookupfile_FollowLinks") == '0' else True):
-        dirs[:] = [i for i in dirs if True not in (fnmatch.fnmatch(i,j)
-                   for j in wildignore['dir'])]
-        for name in files:
-            if True not in (fnmatch.fnmatch(name, j)
-                            for j in wildignore['file']):
-                file_list.append(lfEncode(os.path.join(dir_path,name)))
-            if time.time() - start_time > 120:
-                writelist2file(file_path, file_list)
-                # vim.command('echoerr "FileList:%s"' % ','.join(file_list))
-                # vim.command('let s:file_list = [%s]' % ','.join(file_list))
-                return
-                # return file_list
-    # vim.command('echomsg "%s"' % ','.join(file_list))
-    # return file_list
-    writelist2file(file_path, file_list)
-    # vim.command('let s:file_list = [%s]' % ','.join(file_list))
+    updateFileList(dir_path, file_path, wildignore, linksflag)
 
-def UnitePyGetFileListTest():
-    # dir_path = vim.eval('s:dir_path')
-    # vim.command('echomsg "dir_path:%s"' % dir_path)
-    dir_path = "./"
-    start_time = time.time()
-    wildignore = {"dir":[], "file":[]}
-    file_list = []
-    for dir_path, dirs, files in os.walk(dir_path, followlinks = False):
-        dirs[:] = [i for i in dirs if True not in (fnmatch.fnmatch(i,j)
-                   for j in wildignore['dir'])]
-        for name in files:
-            if True not in (fnmatch.fnmatch(name, j)
-                            for j in wildignore['file']):
-                file_list.append(lfEncode(os.path.join(dir_path,name)))
-            if time.time() - start_time > float(1000):
-                # vim.command('let s:file_list = [%s]' % ','.join(file_list))
-                return
-                # return file_list
-    print(file_list)
-    # return file_list
-    # vim.command('let s:file_list = [%s]' % ','.join(file_list))
-
-# UnitePyGetFileListTest()
